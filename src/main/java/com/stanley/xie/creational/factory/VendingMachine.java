ffb270b86a497a12db379660edd67a2431229fce
GG <GG1D29@users.noreply.github.com>
2025-02-25 10:37:24 +0700

    #4 Factory: Implement the factory method pattern

--- src/main/java/com/stanley/xie/creational/factory/VendingMachine.java
+++ src/main/java/com/stanley/xie/creational/factory/VendingMachine.java
@@ -3,7 +3,16 @@ package com.stanley.xie.creational.factory;
 public class VendingMachine {
 
     Snack getSnack(String snackType) {
-        return null;
+        switch (snackType) {
+            case "ChocolateBar":
+                return new ChocolateBar();
+            case "Chips":
+                return new Chips();
+            case "Drink":
+                return new Drink();
+        }
+
+        throw new IllegalArgumentException("No such snack type: " + snackType);
     }
 
 }
