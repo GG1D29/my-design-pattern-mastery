c4b9d3f0ddd8820c58609b9ccc1344d045156a05
GG <GG1D29@users.noreply.github.com>
2025-02-28 16:27:01 +0700

    #12 Chain of Resp: Implement the CoR pattern.

--- src/main/java/com/stanley/xie/behavioral/chain/ATM.java
+++ src/main/java/com/stanley/xie/behavioral/chain/ATM.java
@@ -1,6 +1,11 @@
 package com.stanley.xie.behavioral.chain;
 
 public abstract class ATM {
+    protected final ATM nextATM;
+
+    public ATM(ATM nextATM) {
+        this.nextATM = nextATM;
+    }
 
     public abstract void dispense(WithdrawalRequest request);
 
--- src/main/java/com/stanley/xie/behavioral/chain/App.java
+++ src/main/java/com/stanley/xie/behavioral/chain/App.java
@@ -3,9 +3,16 @@ package com.stanley.xie.behavioral.chain;
 public class App {
 
     public static void main(String[] args) {
-        var request = new WithdrawalRequest(20, WithdrawalRequest.Currency.USD);
-        var atm = new UsDollarATM();
+        var request = new WithdrawalRequest(20, WithdrawalRequest.Currency.EUR);
+        var atm = buildATM();
         atm.dispense(request);
+    }
+
+    private static ATM buildATM() {
+        var nextATM = new EuroATM(null);
+        var ATM = new UsDollarATM(nextATM);
+
+        return ATM;
     }
 
 }
