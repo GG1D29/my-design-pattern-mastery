a594c40428acf43901173f254aa8cf7d11dbb338
GG <GG1D29@users.noreply.github.com>
2025-03-04 16:29:27 +0700

    #16 Memento: Implement solution using the pattern.

--- src/main/java/com/stanley/xie/behavioral/memento/Item.java
+++ src/main/java/com/stanley/xie/behavioral/memento/Item.java
@@ -3,9 +3,11 @@ package com.stanley.xie.behavioral.memento;
 public class Item {
 
     private int price;
+    private ItemState state;
 
     public Item(int price) {
         this.price = price;
+        this.state = new ItemState(price);
     }
 
     public void setPrice(int price) {
@@ -16,4 +18,11 @@ public class Item {
         return price;
     }
 
+    public void save() {
+        state.setState(price);
+    }
+
+    public void undo() {
+        price = state.getState();
+    }
 }
---
+++ src/main/java/com/stanley/xie/behavioral/memento/ItemState.java
@@ -0,0 +1,17 @@
+package com.stanley.xie.behavioral.memento;
+
+public class ItemState {
+    private int state;
+
+    public ItemState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
