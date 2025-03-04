51bb2cde83c084f4f4d4a4150266f9e6056cfb7e
GG <GG1D29@users.noreply.github.com>
2025-03-04 16:43:26 +0700

    #17 Observer: Implementing the propertyChangeListener class from the JDK

--- src/main/java/com/stanley/xie/behavioral/observer/Newsfeed.java
+++ src/main/java/com/stanley/xie/behavioral/observer/Newsfeed.java
@@ -1,9 +1,12 @@
 package com.stanley.xie.behavioral.observer;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Newsfeed {
+// The Observer.
+public class Newsfeed implements PropertyChangeListener {
 
     private List<String> statuses = new ArrayList<>();
 
@@ -11,4 +14,8 @@ public class Newsfeed {
         statuses.forEach(System.out::println);
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        statuses.add((String) evt.getNewValue());
+    }
 }
--- src/main/java/com/stanley/xie/behavioral/observer/User.java
+++ src/main/java/com/stanley/xie/behavioral/observer/User.java
@@ -1,11 +1,20 @@
 package com.stanley.xie.behavioral.observer;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+// The Observable.
 public class User {
 
     private String status;
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 
     public void setStatus(String status) {
+        changeSupport.firePropertyChange("status", this.status, status);
         this.status = status;
     }
 
+    public void addPropertyChangeListener(Newsfeed newsfeed) {
+        changeSupport.addPropertyChangeListener(newsfeed);
+    }
 }
