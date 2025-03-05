9d01c8a100162dd7434b35f98e13a50e8d702327
GG <GG1D29@users.noreply.github.com>
2025-03-05 10:14:58 +0700

    #17 Observer: Implement observer pattern to the weather app

--- src/main/java/com/stanley/xie/behavioral/observer/weather/Location.java
+++ src/main/java/com/stanley/xie/behavioral/observer/weather/Location.java
@@ -1,11 +1,19 @@
 package com.stanley.xie.behavioral.observer.weather;
 
+import java.beans.PropertyChangeSupport;
+
+//Step2. Implement the Observable class, and adding the Property change listener here.
 public class Location {
 
     private String weatherEvent;
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public void setWeatherEvent(String weatherEvent) {
+        support.firePropertyChange("weatherEvent", this.weatherEvent, weatherEvent);
         this.weatherEvent = weatherEvent;
     }
 
+    public void addPropertyChangeListener(WeatherUpdates weatherUpdates) {
+        support.addPropertyChangeListener(weatherUpdates);
+    }
 }
--- src/main/java/com/stanley/xie/behavioral/observer/weather/WeatherUpdates.java
+++ src/main/java/com/stanley/xie/behavioral/observer/weather/WeatherUpdates.java
@@ -1,9 +1,12 @@
 package com.stanley.xie.behavioral.observer.weather;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WeatherUpdates {
+// Step1. implement the Observer class here.
+public class WeatherUpdates implements PropertyChangeListener {
 
     private List<String> weatherUpdates = new ArrayList<>();
 
@@ -11,4 +14,8 @@ public class WeatherUpdates {
         weatherUpdates.forEach(System.out::println);
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        weatherUpdates.add((String) evt.getNewValue());
+    }
 }
